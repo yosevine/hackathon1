3763344b168d36190b1114ef63083a8b5474ec92
yosevine <yosevine.y@gmail.com>
2022-04-03 17:40:31 +0300

    20220403 at 1740

--- myweeklyactivities.js
+++ myweeklyactivities.js
@@ -25,7 +25,7 @@ for (let i = 0; i < listitems.length; i++) {
         }, 0)
     });
 
-    
+
 
 
 
@@ -42,14 +42,12 @@ for (let i = 0; i < listitems.length; i++) {
 
         list.addEventListener('dragenter', function (e) {
             e.preventDefault();
-            list.style.backgroundColour = 'rgba(0, 0, 0, 0.2)';
         });
 
         // DRAGLEAVE
 
         list.addEventListener('dragleave', function (e) {
             e.preventDefault();
-            list.style.backgroundColour = 'rgba(0, 0, 0, 0.1)';
 
         });
 
@@ -77,21 +75,219 @@ addItemButton.addEventListener("click", function() {
 });
 
 
-// FORM
 
+// FORM
 // form is filled, click submit, form disappear
 
 let todoform = document.getElementById("todoform");
 todoform.addEventListener("submit", function(event) {
-    event.preventDefault (); //prevent the form from autosubmitting
+    event.preventDefault(); //prevent the form from autosubmitting
     todoboxdiv.style.display = "none";
-    
+
+    // save the form in an object array
+    // retrieve the form
+
+    let allToDo = [];
+
+    let itemInfo = document.querySelector("#todo");
+    let itemInfoDes = document.querySelector("#description");
+    let itemInfoDay = document.querySelector("#days");
+
+    let firstInput = itemInfo.value;
+    let secondInput = itemInfoDes.value;
+    let thirdInput = itemInfoDay.value;
+
+    console.log(`${firstInput}`);
+
+    let todoObj = {
+        todo : firstInput,
+        description : secondInput,
+        days : thirdInput
+    };
+
+    console.log(todoObj);
+
+    allToDo.push(todoObj);
+
+    console.log(allToDo);
+    /*
+    create a div
+    append it
+    */
 })
 
+// to create a new div of list item array
+
+let submitForm = document.getElementById("todoform");
+
+submitForm.addEventListener("submit", function() {
+let newDiv = document.createElement("div");
+let day = document.getElementById("monday");
+day.appendChild(newDiv);
+}) 
+
+// console.log(document.getElementById("monday"));
+
+//
+/*
+day.style.display = "block";
+
+
+/*
+
+let submittButton = document.getElementById("submit");
+
+submittButton.addEventListener("click", function() {
+document.createElement("div");
+
+
+}) 
+
+let allList = document.getElementById("monday")
+allList.appendChild("div");
+
+
+/*
+
+let submittButton = document.getElementById("submit");
+submittButton.addEventListener("click" function() {
+    let newDiv = document.createElement("div");
+})
+
+
+let submittButton = document.getElementById("submit");
+submittButton.addEventListener("click", createElement("div");
+console.log(submittButton);
+
+
+todoform.addEventListener("submit", createElement("div"));
+/*
+todoform.addEventListener("submit", function() {
+    todoform.createElement("div");
+    
+})
+console.log(todoform);
+
+
+/*
 todoform.addEventListener("click", function() {
     todoform.style.display = "block";
+});
+/*
+
+let addListItemDiv = document.getElementById("list");
+let addListItem = document.createElement("list-item");
+
+/*
+
+let submitButton = document.querySelector("#submit");
+let addListItem = document.createElement("list-item");
+
+let addListItemDiv = document.getElementById("list");
+
+todoform.addEventListener("submit", function(event) {
+    event.preventDefault ();
+    let addListItem = document.createElement("list-item");
+    addListItemDiv.appendChild(addListItem);
+    addListItem.style.display = "block";
+
+})
+
+console.log(addListItemDiv);
+
+
+/*
+
+let addListItem = document.createElement("list-item");
+let submitButton = document.querySelector("#submit");
+
+
+let addListItemDiv = document.getElementById("list");
+addListItemDiv.appendChild(addListItem);
+console.log(addListItemDiv);
+
+/*
+
+let addListItem = document.querySelector("#list-item");
+let addToDragzone = document.getElementById("list");
+let addListBox = document.getElementsByClassName("lists");
+
+let submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", function() {
+    addToDragzone.createElement("list-item");
+
 
 });
+
+console.log(submitButton);
+
+
+/*
+
+document.div.appendChild(addToDragzone);
+addToDragzone.textContent = "I need to wake up early";
+
+let addTodo = document.createElement("list-item");
+document.body.appendChild(addTodo);
+console.log(addTodo);
+
+
+todoform.addEventListener("submit" function() {
+    addTodo.createElement("list-item");
+    console.log(addTodo);
+})
+
+
+/*
+let addTodo = document.createElement("list-item");
+addTodo.addEventListener("click" function handleClick(event) {
+    console.log("element clicked", event);
+})
+
+
+let addTodo = document.createElement("list-item");
+document.body.appendChild(addTodo);
+
+let submitTodo = document.getElementById("submit");
+    submitTodo.addEventListener("submit", function() {
+
+});
+
+
+
+/*
+
+let submitButton = document.querySelector("#submit");
+let addListItem = document.querySelector("#list-item");
+let addListBox = document.getElementsByClassName("lists");
+
+
+let divItem = document.createElement("div");
+console.log(divItem);
+
+addListItem.appendChild(addListBox);
+document.body.appendChild(addListItem);
+console.log(addListItem);
+
+/*
+
+let submitButton = document.querySelector("#submit");
+let addListitem = document.querySelector("list-item");
+
+submitButton.addEventListener("click", function () {
+
+})
+
+
+
+/*
+
+let submitButton = document.querySelector("submit");
+submitButton.addEventListener("click", function() {
+    submitButton.createElement("list-item");
+});
+
+
 /*
 
 let todoform = document.getElementById("todoform");
